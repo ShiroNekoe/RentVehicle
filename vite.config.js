e54011751212c288e60114785d9dd0ccd783d1aa
ShiroNekoe <davenalk612@gmail.com>
2025-06-03 04:39:49 +0700

    com

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from 'tailwindcss';
-import daisyui from 'daisyui';
-
-// Vite configuration
-export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],  
-      refresh: true,
-    }),
-    tailwindcss(),  // Menggunakan tailwindcss plugin
-    daisyui,        // DaisyUI untuk komponen tambahan
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
   ],
-});
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+};
